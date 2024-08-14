c793cf6a8c2189725096ab5d3d4de11db3b79929
Stavr <rommman@inbox.ru>
2024-08-14 06:41:34 +0300

    Reformat cod

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         BmiService calculate = new BmiService();
-        double bmi = calculate.massa(1.87,98);
-               System.out.println(bmi);
+        double bmi = calculate.massa(1.87, 98);
+        System.out.println(bmi);
     }
 }
